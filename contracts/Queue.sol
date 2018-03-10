99fddb6d6d680996417b515ec11a801498e2f170
Adithya J (AJ) <adithyajay@gmail.com>
2018-03-09 18:21:17 -0800

    queue

--- contracts/Queue.sol
+++ contracts/Queue.sol
@@ -10,32 +10,59 @@ pragma solidity ^0.4.15;
 contract Queue {
 	/* State variables */
 	uint8 size = 5;
-	// YOUR CODE HERE
+	uint numInside;
 
+	bool isFull;
+	bool isEmpty;
+    
+    
+    uint8 first;
+    uint8 lastFilled;
+    uint8 nextFree;
+
+    uint256 duration;
+    
+    mapping (address => uint8) addr2place;
+    mapping (uint8 => address) place2addr;
+    
+    
 	/* Add events */
-	// YOUR CODE HERE
+	event timeEject();
+
+
 
 	/* Add constructor */
-	// YOUR CODE HERE
+	function Queue(uint256 _buytime) {
+        duration = _buytime;
+        numInside = 0;
+    }
 
 	/* Returns the number of people waiting in line */
 	function qsize() constant returns(uint8) {
-		// YOUR CODE HERE
+		return numInside;
 	}
 
 	/* Returns whether the queue is empty or not */
 	function empty() constant returns(bool) {
-		// YOUR CODE HERE
+		return isEmpty;
 	}
 	
 	/* Returns the address of the person in the front of the queue */
 	function getFirst() constant returns(address) {
-		// YOUR CODE HERE
+		return place2addr[0];
 	}
 	
 	/* Allows `msg.sender` to check their position in the queue */
 	function checkPlace() constant returns(uint8) {
-		// YOUR CODE HERE
+        if (addr2place[msg.sender] == 0) {
+        	if (place2addr[0] != msg.sender) {
+        		return 6;
+        	}
+        	else {
+        		return 0;        		
+        	}
+        }
+        return q[msg.sender];
 	}
 	
 	/* Allows anyone to expel the first person in line if their time
@@ -49,11 +76,37 @@ contract Queue {
 	 * they are done with their purchase
 	 */
 	function dequeue() {
-		// YOUR CODE HERE
+		address firstInLine = getFirst();
+		firstPlace = addr2place[firstInLine];
+		delete addr2place[firstInLine];
+		delete place2addr[firstPlace];
+		first = (first + 1) % size;
+		numInside-=1;
+
+		if (numInside == 0) {
+			isEmpty = true;
+
+		}
 	}
 
 	/* Places `addr` in the first empty position in the queue */
 	function enqueue(address addr) {
-		// YOUR CODE HERE
-	}
+		if (isFull) {
+			revert();
+
+		}
+		
+		addr2place[addr] = nextFree;
+		place2addr[nextFree] = addr;
+		lastFilled = nextFree;
+		nextFree = (lastFilled+1) % size;
+		numInside+=1;
+
+		if (nextFree >= first) {
+			isFull = true;
+		}
+
+	}    
+
+
 }
